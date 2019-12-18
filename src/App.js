163c9103b1f718dbc9221f50c5d483412fd4b437
Johan <johanespino9@gmail.com>
2019-12-18 18:24:12 -0500

    [UPDATE] Show customers list

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ApolloProvider } from 'react-apollo'
 import ApolloClient from 'apollo-boost'
 
 import Header from './componentes/Header'
+import Clientes from './componentes/Clientes'
 
 const client = new ApolloClient({
   uri: 'http://localhost:5600/graphql',
@@ -16,6 +17,9 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Header/>
+      <div className="container">
+        <Clientes/>
+      </div>
     </ApolloProvider>
   )
 }
--- src/componentes/Clientes.js
+++ src/componentes/Clientes.js
@@ -1,16 +1,34 @@
-import  React from 'react'
+import  React, { Fragment } from 'react'
 import { Query } from 'react-apollo'
 
-import { CLIENTES_QUERY } from './queries'
+import { CLIENTES_QUERY } from '../queries'
 
 const Contactos = () => (
   <Query query={CLIENTES_QUERY}>
     {({ loading, error, data }) => {
       if(loading) return 'Cargando...'
       if(error) return `Error: ${error.message}`
-      console.log(data)
+      console.log(data.getClientes)
       return (
-        <h2 className="text-center">Listado Clientes</h2>
+        <Fragment>
+          <h2 classname="text-center">Listado Clientes</h2>
+          <ul classname="list-group">
+            {data.getClientes.map(item => (
+              <li key={item.id} className="list-group-item">
+                <div className="row justify-content-between align-items-center">
+                  <div className="col-md-8 d-flex justify-content-between align-items-center">    
+                    {item.nombre} {item.apellido}
+                  </div>
+                  <div className="col-md-4 d-flex justify-content-end">
+                    <a className="btn btn-success d-block d-md-inline-block">
+                      Editar Cliente
+                    </a>
+                  </div>
+                </div>
+              </li>
+      ))}
+          </ul>
+        </Fragment>
       )
     }}
 
